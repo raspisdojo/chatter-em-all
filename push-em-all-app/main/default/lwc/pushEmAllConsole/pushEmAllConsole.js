0590c1b57be151c346bc3b09da4e7583df094887
María <maria_aparicio_abad@outlook.com>
2020-10-10 02:08:39 +0200

    Added lookup event

--- push-em-all-app/main/default/lwc/pushEmAllConsole/pushEmAllConsole.js
+++ push-em-all-app/main/default/lwc/pushEmAllConsole/pushEmAllConsole.js
@@ -1,3 +1,14 @@
-import { LightningElement } from "lwc";
+import { LightningElement, api } from "lwc";
 
-export default class PushEmAllConsole extends LightningElement {}
+export default class PushEmAllConsole extends LightningElement {
+    @api record;
+
+    createNotification() {
+        console.log("Create notification to: " + this.record.id);
+    }
+
+    lookupChange(e) {
+        this.record = e.detail;
+        console.log(JSON.stringify(this.record, null, 4));
+    }
+}
