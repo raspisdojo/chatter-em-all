7dbd2cf58404741ec4394fe4b5810fb5adbb0678
María <maria_aparicio_abad@outlook.com>
2020-10-16 23:48:01 +0200

    Send push notification!

--- push-em-all-app/main/default/lwc/pushEmAllConsole/pushEmAllConsole.js
+++ push-em-all-app/main/default/lwc/pushEmAllConsole/pushEmAllConsole.js
@@ -1,15 +1,36 @@
 import { LightningElement, api } from "lwc";
+import sendPushNotification from "@salesforce/apex/PushEmAllConsoleController.sendPushNotification";
 
 export default class PushEmAllConsole extends LightningElement {
-    @api record;
+    recipientIds = new Array();
+    _error;
 
     createNotification() {
-        console.log("Create notification to: " + this.record.id);
+        let title = this.template.querySelector("lightning-input").value;
+        let body = this.template.querySelector("lightning-textarea").value;
+        console.log("Create notification to: " + this.recipientIds[0]);
+        console.log(title);
+        console.log(body);
+        sendPushNotification({
+            notificationInfo: {
+                title: title,
+                body: body,
+                recipientIds: this.recipientIds
+            }
+        })
+            .then((result) => {
+                console.log(result);
+            })
+            .catch((error) => {
+                this._error = error;
+                console.log(error);
+            });
     }
 
     lookupChange(e) {
         // eslint-disable-next-line @lwc/lwc/no-api-reassignments
-        this.record = e.detail;
-        console.log(JSON.stringify(this.record, null, 4));
+        let record = e.detail;
+        console.log(JSON.stringify(record, null, 4));
+        if (record != null) this.recipientIds[0] = record.id;
     }
 }
